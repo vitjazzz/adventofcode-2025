efda648a3f2646e2a2dd71cce7f38326747b0562
vzikratyi <vzikratyi@playtika.com>
2026-04-10 15:51:12 +0300

    Day 7. Part 1

---
+++ src/main/java/Day7.java
@@ -0,0 +1,103 @@
+import java.util.List;
+
+import utils.EnvLoader;
+import utils.HttpInputFetcher;
+
+public class Day7 {
+
+    private static final String URL = "https://adventofcode.com/2025/day/7/input";
+    private static final String TEST_DATA = """
+            .......S.......
+            ...............
+            .......^.......
+            ...............
+            ......^.^......
+            ...............
+            .....^.^.^.....
+            ...............
+            ....^.^...^....
+            ...............
+            ...^.^...^.^...
+            ...............
+            ..^...^.....^..
+            ...............
+            .^.^.^.^.^...^.
+            ...............
+              """;
+
+
+    // wrongs pt.1:
+    // wrongs pt.2:
+    public static void execute() {
+        var lines = linesFromTestData();
+//        var lines = linesFromUrl();
+        char[][] grid = parseGrid(lines);
+        Point start = findStart(grid);
+        int[][] counts = new int[grid.length][grid[0].length];
+
+        System.out.println(calculateSplits(start, grid, counts));
+    }
+
+
+    private static int calculateSplits(Point p, char[][] grid, int[][] counts) {
+        if (p.j < 0 || p.j >= grid[0].length) {
+            return 0;
+        }
+        int i = p.i;
+        for (; i <= grid.length; i++) {
+            if (i == grid.length) {
+                return 0;
+            }
+            if (grid[i][p.j] == '^') {
+                break;
+            }
+        }
+        if (counts[i][p.j] > 0) {
+            return counts[p.i][p.j];
+        }
+        int splits = 1
+                + calculateSplits(new Point(i, p.j - 1), grid, counts)
+                + calculateSplits(new Point(i, p.j + 1), grid, counts);
+        counts[i][p.j] = splits;
+        return splits;
+    }
+
+    private static List<String> linesFromUrl() {
+        return new HttpInputFetcher(EnvLoader.loadCookie()).fetchLines(URL).stream()
+                .toList();
+    }
+
+    private static List<String> linesFromTestData() {
+        return TEST_DATA.lines().toList();
+    }
+
+    private static char[][] parseGrid(List<String> lines) {
+        int rows = lines.size();
+        int cols = lines.get(0).length();
+
+        char[][] grid = new char[rows][cols];
+
+        for (int i = 0; i < rows; i++) {
+            grid[i] = lines.get(i).toCharArray();
+        }
+
+        return grid;
+    }
+
+    private static Point findStart(char[][] grid) {
+        int startRow = -1, startCol = -1;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == 'S') {
+                    startRow = i;
+                    startCol = j;
+                }
+            }
+        }
+        return new Point(startRow, startCol);
+    }
+
+    private record Point(int i, int j) {
+    }
+}
--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,7 +1,7 @@
 public class MainClass {
 
     public static void main(String[] args) {
-       Day6.execute();
+       Day7.execute();
     }
 
 }
